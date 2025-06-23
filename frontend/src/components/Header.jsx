4a1c3ecd840f6589ec55d3e71bcf85bd48e421db
sananahmad7 <sananahmad909@gmail.com>
2025-06-23 22:32:09 +0500

    Worked on Home Page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <div className='flex  justify-between bg-white border' >
-            <div className='border flex-row ml-10'>
-                <img className='h-25 w-40' src='src/assets/Logo.png' alt='idk' />
+        <div className='flex  justify-between items-center bg-white border mx-15' >
+            <div className='border flex-row'>
+                <img className='h-30 w-45' src='src/assets/Logo.png' alt='idk' />
                 <img src='src/assets/Name.png' alt='idk' />
             </div>
-            <div className='border p-10 space-x-10 mr-10' >
-                <Link className='text-2xl font-bold text-indigo-800 hover:text-red-600 transition duration-200
+            <div className='border p-10 space-x-10' >
+                <Link className='text-3xl font-bold text-indigo-800 hover:text-red-600 transition duration-200
 					 ease-in-out' to="/">Home</Link>
-                <Link className='text-2xl font-bold text-indigo-800 hover:text-red-600 transition duration-200' to="/global">Global Coverage</Link>
-                <Link className='text-2xl font-bold text-indigo-800 hover:text-red-600 transition duration-200' to="/about">About</Link>
-                <Link className='text-2xl font-bold text-indigo-800 hover:text-red-600 transition duration-200' to="/news">News</Link>
+                <Link className='text-3xl font-bold text-indigo-800 hover:text-red-600 transition duration-200' to="/global">Global Coverage</Link>
+                <Link className='text-3xl font-bold text-indigo-800 hover:text-red-600 transition duration-200' to="/about">About</Link>
+                <Link className='text-3xl font-bold text-indigo-800 hover:text-red-600 transition duration-200' to="/news">News</Link>
             </div>
         </div>
     )
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function HomePage() {
     return (
-        <div>HomePage</div>
+        <div className='border h-screen '>
+            <div>
+                <img className='w-full lg:h-160 xl:h-20 2xl:h-280 2.5xl:h-300' src='../src/assets/Truck.jpg' />
+
+            </div>
+        </div>
     )
 }
 
